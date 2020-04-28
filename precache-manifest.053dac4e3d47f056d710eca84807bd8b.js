e48792857afd228a9afaffbf2697cee5d9bca5eb
Mark Whang <hwhang0917@gmail.com>
2020-04-29 01:42:56 +0900

    Updates

---
+++ precache-manifest.053dac4e3d47f056d710eca84807bd8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a846a799fdf96894e4833b11e66e184a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4e473bc71da0b4cfeb5",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "fa3b4fdf7134aeef0dbf",
+    "url": "/static/css/main.5b43b5bc.chunk.css"
+  },
+  {
+    "revision": "d4e473bc71da0b4cfeb5",
+    "url": "/static/js/2.ee0e22a9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ee0e22a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa3b4fdf7134aeef0dbf",
+    "url": "/static/js/main.c93f1e3c.chunk.js"
+  },
+  {
+    "revision": "181fd42d8ca1e9842422",
+    "url": "/static/js/runtime-main.d51104c2.js"
+  }
+]);
